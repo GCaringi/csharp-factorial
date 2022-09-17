5a750e7adaa1f70e35970edcbc436d12aff4daa4
GCaringi <giannicaringi94@gmail.com>
2022-09-17 15:50:06 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+Console.Write("Inserisci un numero di cui vuoi calcolare il fattoriale: ");
+int factNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Fattoriale di {factNum}");
+Console.WriteLine(factorial(factNum));
+
